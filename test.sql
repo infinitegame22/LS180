66bebf545b54c491dd4c4f7d9fd41e121820d6d5
Amy Dripchak <rejuvenatingserendipity@gmail.com>
2023-11-07 17:56:14 -0700

    update

---
+++ test.sql
@@ -0,0 +1,10 @@
+CREATE TABLE some_table();
+
+CREATE TABLE table_name (
+    column_1_name column_1_data_type [constraints, ...],
+    column_2_name column_2_data_type [constraints, ...],
+    .
+    .
+    .
+    constraints
+);
